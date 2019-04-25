f90b3edb0f61de1fb9bc0607231140421dab5bde
Maryna <masini4ka@gmail.com>
2019-04-25 15:11:47 +0300

    heroku4

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/{{ang-movies}}'));
+app.use(express.static('./dist/ang-movies'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname,'/dist/{{ang-movies}}/index.html'));
+  res.sendFile(path.join(__dirname,'/dist/ang-movies/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
